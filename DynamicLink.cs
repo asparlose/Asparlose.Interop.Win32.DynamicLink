336da426515822819d0b650bab2e63bc3349d18c
Asparlose <asparlose@gmail.com>
2018-05-17 06:24:47 +0900

    :star2: DynamicLink, DynamicLinkFromBinary

---
+++ DynamicLink.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Asparlose.Interop.Win32
+{
+    public abstract class DynamicLink : DisposableObject
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("kernel32", SetLastError = true)]
+        internal static extern bool FreeLibrary(IntPtr hModule);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = false)]
+        internal static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        readonly IntPtr lib;
+        protected string FilePath { get; }
+
+        protected DynamicLink(string dll)
+        {
+            FilePath = dll;
+            lib = LoadLibrary(dll);
+
+            if (lib == IntPtr.Zero)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        protected T GetProc<T>(string name)
+        {
+            var addr = GetProcAddress(lib, name);
+            if (addr == IntPtr.Zero)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            return Marshal.GetDelegateForFunctionPointer<T>(addr);
+        }
+
+        protected override void ReleaseUnmanagedResources()
+        {
+            base.ReleaseUnmanagedResources();
+
+            FreeLibrary(lib);
+        }
+
+    }
+}
---
+++ DynamicLinkFromBinary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Asparlose.Interop.Win32
+{
+    public abstract class DynamicLinkFromBinary : DynamicLink
+    {
+        static string CreateTemporaryFile()
+        {
+            var tempPath = Path.GetTempPath();
+
+            while (true)
+            {
+                var filename = Path.Combine(tempPath, Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".dll");
+                try
+                {
+                    using (var s = File.Open(filename, FileMode.CreateNew))
+                    {
+                        return filename;
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        static string WriteToTemporaryFile(byte[] bytes)
+        {
+            var fileName = CreateTemporaryFile();
+            using (var s = File.Create(fileName))
+            {
+                s.Write(bytes, 0, bytes.Length);
+            }
+            return fileName;
+        }
+
+        protected DynamicLinkFromBinary(byte[] dll) : base(WriteToTemporaryFile(dll))
+        {
+            Disposed += DynamicLinkFromBinary_Disposed;
+        }
+
+        private void DynamicLinkFromBinary_Disposed(object sender, EventArgs e)
+        {
+            File.Delete(FilePath);
+        }
+    }
+}
